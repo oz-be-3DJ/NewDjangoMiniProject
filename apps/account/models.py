8b75bf6ee000e57829b152cd880d1534603e73d4
명보경 <zzazan0320@gmail.com>
2025-04-03 14:21:27 +0900

    account 계좌 번호 수정 완료했습니다.

--- apps/account/models.py
+++ apps/account/models.py
@@ -7,7 +7,7 @@ class Account(models.Model):
     id = models.AutoField(primary_key=True)
     #user = models.ForeignKey(User, on_delete=models.CASCADE) # 유저와 테이블
     #bank = models.ForeignKey(Bank, on_delete=models.CASCADE) # 은행 코드 (은행은 여러 개의 계좌를 가질 수 있음)
-    account_number = models.CharField(max_length=20) # 계좌 번호
+    account_number = models.CharField(max_length=20, unique=True) # 계좌 번호
     account_type = models.CharField(max_length=15) # 계좌 종류 (입출금, 저축, 예금 등)
     balance = models.DecimalField(max_digits=12, decimal_places=2) # 계좌 잔액
     created_at = models.DateTimeField(auto_now_add=True) # 계좌 생성일 (객체 생성 때 자동으로 설정)
